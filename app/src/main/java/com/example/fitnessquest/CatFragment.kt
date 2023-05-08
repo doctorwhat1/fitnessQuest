48dfd1859621d146f00bf1d37d3a20663c678f68
vsmorkovkin-pc <vsmorkovkin@edu.hse.ru>
2023-05-08 22:26:35 +0300

    cat screen v0.2(filled cards in catFragment)

--- app/src/main/java/com/example/fitnessquest/CatFragment.kt
+++ app/src/main/java/com/example/fitnessquest/CatFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fitnessquest.databinding.FragmentCatBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class CatFragment : Fragment() {
@@ -19,7 +21,6 @@ class CatFragment : Fragment() {
     ): View? {
         _binding = FragmentCatBinding.inflate(inflater, container, false)
         val view = binding.root
-
 
         return view
     }
