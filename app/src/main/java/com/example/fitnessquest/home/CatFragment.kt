48a0ece431bd836c7df72cab6a58e3f697d7dfa3
vsmorkovkin-pc <vsmorkovkin@edu.hse.ru>
2023-05-18 19:34:38 +0300

    added fragments for cards

--- app/src/main/java/com/example/fitnessquest/home/CatFragment.kt
+++ app/src/main/java/com/example/fitnessquest/home/CatFragment.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.fitnessquest.R
 import com.example.fitnessquest.databinding.FragmentCatBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -34,6 +36,29 @@ class CatFragment : Fragment() {
 
         binding.catViewModel = viewModel // set the data binding variable
         binding.lifecycleOwner = viewLifecycleOwner // lets the layout respond to live data updates
+
+        binding.cvStrolls.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_strollsFragment)
+        }
+        binding.cvFood.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_foodFragment)
+        }
+        binding.cvActivity.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_activityFragment)
+        }
+        binding.cvWater.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_waterFragment)
+        }
+        binding.cvSleep.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_sleepFragment)
+        }
+        binding.cvWeight.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_weightFragment)
+        }
+
+        binding.btnInputWeight.setOnClickListener {
+            view.findNavController().navigate(R.id.action_catFragment_to_weightFragment)
+        }
 
         return view
     }
